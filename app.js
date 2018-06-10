302d44154c321c98ccebe5bdfb5573d9b3763ac1
Elisheva-Sosnovitch-Behar <esosno@gmail.com>
2018-06-10 13:50:07 +0300

    Simplified but functional version of cat clicker duo

--- app.js
+++ app.js
@@ -1,51 +1,18 @@
-class CatClicker {
-    constructor (name, pic){
-        this.name = name;
-        this.pic = pic; 
-    }
-    
-    buildCat() {
-        const catDiv = document.createElement('DIV');
-        const divName = document.createElement('P').setAttribute("class","catName");
-        const divPic = document.createElement('IMG').setAttribute("class","catImage");
-        const divCounter = document.createElement('P').setAttribute("class","counter");
-        catDiv.append(divName, divPic, divCounter);
-    }
-    
-    nameMe() {
-        let catName = this.getElementsByClassName('catName').innerText;         
-        catName = this.name;
-    }
-    
-    picMe() {
-        let catPic = document.getElementsByClassName('catImage');
-        catPic.setAttribute("src", this.pic);
-        catPic.setAttribute("alt", "cat head");
-    }
-    
-    catClick() {
-        const counter = this.getElementsByClassName('counter');
-        counter.innerText = 0;
-        let counterNumber = 0;
-        const catImage = this.getElementsByClassName('catImage');
-        catImage.addEventListener('click', function () {
-        ++counterNumber;
-        counter.innerHTML= counterNumber;
+
+const counter1 = document.getElementById('counter1');
+counter1.innerText = 0;
+let counter1Number = 0;
+const catImage1 = document.getElementById('cat1');
+catImage1.addEventListener('click', function () {
+++counter1Number;
+counter1.innerHTML= counter1Number;
         });
-    }
-}
 
-
-let cat1 = new CatClicker('Biscuit', "japanese-bobtail.png");
-let cat2 = new CatClicker('Ginger', "turkish-van.png");
-let catArray = [cat1, cat2];
-        
-const catNode = document.getElementById('catNode');
-
-for (cat of catArray){
-    cat.buildCat();
-    cat.nameMe();
-    cat.picMe();
-    cat.catClick();
-    catNode.appendChild(cat);
-};
+const counter2 = document.getElementById('counter2');
+counter2.innerText = 0;
+let counter2Number = 0;
+const catImage2 = document.getElementById('cat2');
+catImage2.addEventListener('click', function () {
+++counter2Number;
+counter2.innerHTML= counter2Number;
+        });

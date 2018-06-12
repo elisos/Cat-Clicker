23834f465e4031140099acef8aa665df61edc46f
Elisheva-Sosnovitch-Behar <esosno@gmail.com>
2018-06-12 10:25:39 +0300

    Much confusion

--- app.js
+++ app.js
@@ -1,18 +1,74 @@
+const catName = document.getElementById('catName').innerText;
+const catImage = document.getElementsByClassName('catImage');
+const counter = document.getElementById('counter');
+counter.innerText = 0;
+const menu = document.getElementById("catNav");
+const catTabs = menu.querySelectorAll("li");
+const tabArray = [...catTabs];
 
-const counter1 = document.getElementById('counter1');
-counter1.innerText = 0;
-let counter1Number = 0;
-const catImage1 = document.getElementById('cat1');
-catImage1.addEventListener('click', function () {
-++counter1Number;
-counter1.innerHTML= counter1Number;
-        });
+menu.addEventListener("click", chooseCat);
 
-const counter2 = document.getElementById('counter2');
-counter2.innerText = 0;
-let counter2Number = 0;
-const catImage2 = document.getElementById('cat2');
-catImage2.addEventListener('click', function () {
-++counter2Number;
-counter2.innerHTML= counter2Number;
+function chooseCat(e) {
+   for (tab of tabArray){
+       tab.classList.remove('active');
+   }
+        e.target.classList.add('active');
+}
+
+class Cat {
+    constructor (name, pic, clickCount = 0, tab) {
+        this.name = name;
+        this.pic = pic; 
+        this.clickCount = clickCount;
+        this.tab = tab;
+    }
+    
+    populate() {
+        menu.addEventListener('click', function(e) {
+            if (e.target === this.tab){
+            catName = this.name;
+            catImage.setAttribute('src', this.pic);
+//            this.nameMe();
+//            this.picMe();
+            }
         });
+    }
+    
+//    nameMe() {
+//        catName = this.name;
+//    }
+//    
+//    picMe() {
+//        catImage.setAttribute('src', this.pic);
+//    }
+    
+    catClick() {
+        catImage.addEventListener('click', function (picCopy) {
+            return function () {
+                ++this.clickCount;
+                counter.innerText= this.clickCount;
+                catImage.classList.add('animated', 'pulse');
+                setTimeout(function () {
+                    catImage.classList.remove('animated', 'pulse');
+                }, 200);
+            };
+        })(this.pic);
+    }
+}
+//
+//
+let biscuit = new Cat('Biscuit', "biscuit.png", tabArray[0]);
+//let ginger = new Cat('Ginger', "ginger.png",  tabArray[1]);
+//let snowy = new Cat('Snowy', "<img src="snowy.png" alt="cat head" id="snowyImage" class="catImage">", tabArray[2]);
+//let goblin = new Cat('Goblin', "<img src="goblin.png" alt="cat head" id="goblinImage" class="catImage">", tabArray[3]);
+//let mitzi = new Cat('Mitzi', "<img src="mitzi.png" alt="cat head" id="mitziImage" class="catImage">", tabArray[4]);
+//let gingko = new Cat('Gingko', "<img src="gingko.png" alt="cat head" class="catImage" id="gingkoImage">", tabArray[5]);
+let catArray = [biscuit, ginger, snowy, goblin, mitzi, gingko];
+
+for (cat of catArray){
+    cat.populate();
+//    cat.catClick();
+};
+
+//"<img src=/"biscuit.png/" alt=/"cat head/" id=/"biscuitImage/" class=/"catImage/">"
+//"<img src="ginger.png" alt="cat head" id="gingerImage" class="catImage">",
